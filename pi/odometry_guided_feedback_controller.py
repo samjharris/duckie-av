b7eb5d16a6c6f24b11ebae44c08d7695d211b7e4
Alex Behr <abehr@umass.edu>
2019-10-23 11:04:57 -0400

    updated odometry_guided_feedback_controller.py with correct F_pd and comments

--- pi/odometry_guided_feedback_controller.py
+++ pi/odometry_guided_feedback_controller.py
@@ -1,22 +1,17 @@
+# CICS 503 Fall 2019 DuckieTown Group 4
 # Odometry-Guided Feedback Controller
-# translate and rotate to minimize the error between where the robot thinks it
-# is and where it should be, i.e.:
-# Fcontrol = -K((x,y)act - (x,y)ref) - B((x_vel,y_vel)act)
-# map the Fcontrol to a PWM command for both wheels. Tune the values
-# of K and B to make this work the way you want it to.
 #
 # Inputs:
 #         x_ref(t)   = function taking time returning triple:
 #                                                   [x_ref, y_ref, theta_ref]
+#                      theta is expected in degrees, not radians
 #         x_act      = [x_act, y_act, theta_act]
 #         t          = time, int
 #         PWM_R_prev = voltage, scalar
 #         PWM_L_prev = voltage, scalar
 #
 # Returns:
-#         PWM_R_new = voltage, scalar
-#         PWM_L_new = voltage, scalar
-
+#         (PWM_R_new, PWM_L_new) = tuple of voltages
 
 import numpy as np
 
@@ -28,25 +23,35 @@ def get_PWMs(x_ref_func, t, x_act, PWM_L_prev, PWM_R_prev):
     # yoke point distance from center of the wheel base
     r_length = 1
 
-    # I is some function of k and B
+    # spring and damper constants and I (what's I?)
+    k = 1
+    B = 1
     I = 1
 
+    # The target in the world frame
     x_ref = x_ref_func(t)
-
-    # Direction from bot to x_ref
-    F_pd = np.array([x_ref[0] - x_act[0], x_ref[1] - x_act[1]])
 
     # Unit vector in direction of x_act
     x_r = np.array([np.cos(np.deg2rad(x_act[2])), np.sin(np.deg2rad(x_act[2]))])
 
-    # Should we include k here??
+    # F_pd as given by the spring / damper function
+    # F_pd = -k((x,y)act - (x,y)ref) - B((x_speed, y_speed)act)
+    speed = (PWM_L_prev + PWM_R_prev) / 2
+    world_frame_velocity = np.array([speed * x_r[0], speed * x_r[1]])
+    F_pd = -k * (x_act[0:2] - x_ref[0:2]) - B * (world_frame_velocity)
+
+    # F_trans = <F_pd, x_r> / m = delta_PWM_trans
     delta_PWM_trans = np.dot(F_pd, x_r) / m
 
-    # Cross product to get M_rot
+    # M_rot = r cross F_pd
+    # delta_theta_dot = M_rot / I = r cross F_pd
+    # delta_PWM_rot = delta_theta_dot * r_length
     r = x_r * r_length
     M_rot = np.cross(r, F_pd)
     delta_PWM_rot = r_length * (M_rot / I)
 
+    # Add delta PWMs to previous values to obtain new values
+    # Subtract rotational term from left and add to right (right hand rule)
     PWM_L_new = PWM_L_prev + delta_PWM_trans - delta_PWM_rot
     PWM_R_new = PWM_R_prev + delta_PWM_trans + delta_PWM_rot
 
@@ -59,5 +64,3 @@ def test():
     test_x_act = np.array([1,-1,-15])
     result = get_PWMs(x_ref_dumb_func, 1, test_x_act, 0, 0)
     print(result)
-
-test()
