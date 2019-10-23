fixed formula & added notes

pi/odometry_guided_feedback_controller.py
@@ -1,6 +1,9 @@
 # CICS 503 Fall 2019 DuckieTown Group 4
 # Odometry-Guided Feedback Controller
-#
+
+import numpy as np
+
+
 # Inputs:
 #         x_ref(t)   = function taking time returning triple:
 #                                                   [x_ref, y_ref, theta_ref]
@@ -11,22 +14,23 @@
 #         PWM_L_prev = voltage, scalar
 #
 # Returns:
-#         (PWM_R_new, PWM_L_new) = tuple of voltages
-
-import numpy as np
-
+#         (PWM_R_new, PWM_L_new) = pair of motor control signals in the range [-400, 400]
 def get_PWMs(x_ref_func, t, x_act, PWM_L_prev, PWM_R_prev):
 
-    # mass of the robot
-    m = 1
+    # TODO: measure this
+    # mass of the robot (kilograms)
+    m = 1.0
 
-    # yoke point distance from center of the wheel base
-    r_length = 1
+    # TODO: measure this
+    # yoke point distance from center of the wheel base (meters)
+    r_length = 1.0
 
-    # spring and damper constants and I (what's I?)
-    k = 1
-    B = 1
-    I = 1
+    K = 1  # spring constant
+    B = 1  # damper constant
+
+    # TODO: check this formula
+    # https://en.wikipedia.org/wiki/Moment_of_inertia
+    I = m*r_length*r_length
 
     # The target in the world frame
     x_ref = x_ref_func(t)
@@ -35,10 +39,10 @@ def get_PWMs(x_ref_func, t, x_act, PWM_L_prev, PWM_R_prev):
     x_r = np.array([np.cos(np.deg2rad(x_act[2])), np.sin(np.deg2rad(x_act[2]))])
 
     # F_pd as given by the spring / damper function
-    # F_pd = -k((x,y)act - (x,y)ref) - B((x_speed, y_speed)act)
+    # F_pd = -K((x,y)act - (x,y)ref) - B((x_speed, y_speed)act)
     speed = (PWM_L_prev + PWM_R_prev) / 2
     world_frame_velocity = np.array([speed * x_r[0], speed * x_r[1]])
-    F_pd = -k * (x_act[0:2] - x_ref[0:2]) - B * (world_frame_velocity)
+    F_pd = -K * (x_act[0:2] - x_ref[0:2]) - B * (world_frame_velocity)
 
     # F_trans = <F_pd, x_r> / m = delta_PWM_trans
     delta_PWM_trans = np.dot(F_pd, x_r) / m
@@ -57,8 +61,10 @@ def get_PWMs(x_ref_func, t, x_act, PWM_L_prev, PWM_R_prev):
 
     return (PWM_L_new, PWM_R_new)
 
+
 def x_ref_dumb_func(t):
     return np.array([2,0,0])
+
 
 def test():
     test_x_act = np.array([1,-1,-15])