f6b5f0a4b4c0adbd87c4e1b264322039191b7854
Alex Behr <abehr@umass.edu>
2019-10-24 12:46:45 -0400

    added time tracking to closed_loop_controller.py

--- pi/closed_loop_controller.py
+++ pi/closed_loop_controller.py
@@ -11,9 +11,11 @@
 #     update x_act
 
 import numpy as pd
+import time
 import positions
 from  odometry_guided_feedback_controller import get_PWMs
 
+timeslice = .25
 x_act = [0,0,0]
 PWM_L, PWM_R, time = 0, 0, 0
 x_ref = positions.get_x_ref_func_one_meter()
@@ -21,6 +23,7 @@ x_ref = positions.get_x_ref_func_one_meter()
 while true:
     PWM_L, PWM_R = get_PWMs(x_ref, time, PWM_L, PWM_R)
     # send to PWMs to Arduino
+    time.sleep(timeslice)
     # wait for input from Arduino: wheel distances as dist_l and dist_r
     x_act_new = positions.get_x_act_new(x_act, dist_l, dist_r)
     if x_act == x_act_new:
