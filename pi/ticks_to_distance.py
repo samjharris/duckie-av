e5eabfc78b30bf55c4efcaaf417576b3d63b4b13
ch403 <cwhuang@umass.edu>
2019-10-24 18:14:19 -0400

    move code communication back to ticks_to_distance.py for now

--- pi/ticks_to_distance.py
+++ pi/ticks_to_distance.py
@@ -1,3 +1,9 @@
+import serial
+import serial.tools.list_ports as list_ports
+import struct
+import time
+import sys
+
 distance_per_tick = 22.0/26.0
 
 def tick_to_centimeter(ticks):
@@ -9,3 +15,54 @@ def get_distances(left_ticks, right_ticks):
 	right_distance = tick_to_centimeter(right_ticks)
 	return left_distance, right_distance
 
+# sys.path.append('../pi')
+# import ticks_to_distance as ticks_to_distance
+
+## connect to the open serial port
+ports = list(list_ports.comports())
+ports = [p[0] for p in ports]
+if len(ports) == 0:
+    print("error, couldn't find any open ports")
+    exit()
+print("found open ports: {}".format(ports))
+
+## set the baud rate - this must match the uno's
+baud_rate = 115200
+#baud_rate = 9600
+
+##initialize our serial connection
+s_con = serial.Serial(ports[0], baud_rate)
+s_con.flushInput()
+
+comp_list = ["Motor Values? Pi\r\n"]
+##begin control loop
+while True:
+	if s_con.inWaiting() > 0:
+
+		inputValue = s_con.readline()
+
+		
+		# n = input("Set arduino flash times:")
+		print(inputValue)
+		if inputValue in comp_list:
+			try:
+				right_value = 20
+				left_value = 50
+
+				s_con.write(struct.pack('>BB', right_value, left_value))
+			except:
+				print("error passing motor values")
+				s_con.write('0')
+
+
+		if len(inputValue.split()) != 2:
+			continue
+
+		love = inputValue.strip()
+		left_ticks, right_ticks = [int(x) for x in love.split()]
+		# print(left_ticks, right_ticks)
+
+		left_distance, right_distance = get_distances(left_ticks, right_ticks)
+		print("distances: ", left_distance, right_distance)
+
+		time.sleep(0.2)
--- serial_communication/pi_comshandler.py
+++ serial_communication/pi_comshandler.py
@@ -1,65 +1,9 @@
 import serial
 import serial.tools.list_ports as list_ports
-import struct
-import time
-import sys
-sys.path.append('../pi')
-import ticks_to_distance as ticks_to_distance
-
-<<<<<<< HEAD
-## connect to the open serial port
-ports = list(list_ports.comports())
-ports = [p[0] for p in ports]
-if len(ports) == 0:
-    print("error, couldn't find any open ports")
-    exit()
-print("found open ports: {}".format(ports))
-
-## set the baud rate - this must match the uno's
-baud_rate = 115200
-#baud_rate = 9600
-
-##initialize our serial connection
-s_con = serial.Serial(ports[0], baud_rate)
-s_con.flushInput()
-
-comp_list = ["Motor Values? Pi\r\n"]
-##begin control loop
-while True:
-	if s_con.inWaiting() > 0:
-
-		inputValue = s_con.readline()
-
-		
-		# n = input("Set arduino flash times:")
-		print(inputValue)
-		if inputValue in comp_list:
-			try:
-				right_value = 20
-				left_value = 50
-
-				s_con.write(struct.pack('>BB', right_value, left_value))
-			except:
-				print("error passing motor values")
-				s_con.write('0')
-
-
-		if len(inputValue.split()) != 2:
-			continue
-
-		love = inputValue.strip()
-		left_ticks, right_ticks = [int(x) for x in love.split()]
-		# print(left_ticks, right_ticks)
-
-		left_distance, right_distance = ticks_to_distance.get_distances(left_ticks, right_ticks)
-		print("distances: ", left_distance, right_distance)
-
-		time.sleep(0.2)
-
 
 def sendData(data):
 	return
-=======
+
 ##Serial connection object definition
 class SerialCon():
 	def __init__(self):
@@ -114,4 +58,3 @@ x=0
 y=0
 x=mySerial.read()
 mySerial.write(y)
->>>>>>> a27915ed0569e9b0a0f86712197b4d6153727822
