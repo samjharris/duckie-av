397c11fd9a2eb9e693eaac04a341983fd0e6d10c
Sam Harris <sjharris@umass.edu>
2019-10-24 20:07:17 -0400

    Added serial comms to CL controller

--- pi/closed_loop_controller.py
+++ pi/closed_loop_controller.py
@@ -10,9 +10,10 @@
 #     get left and right distance deltas from arduino
 #     update x_act
 
-import numpy as pd
+import numpy as np
 import time
 import positions
+from pi_serial import SerialCon 
 from  odometry_guided_feedback import get_PWMs
 
 timeslice = .25
@@ -20,12 +21,28 @@ x_act = [0,0,0]
 PWM_L, PWM_R, curr_time = 0, 0, 0
 x_ref = positions.get_x_ref_func_one_meter()
 
+##Initialize serial communication
+sc = SerialCon()
+ports = sc.getOpenPorts()
+if not ports:
+	print("No open serial ports, exiting")
+	exit()
+#Select & set appropriate port
+port = ports[0]
+sc.setPort(port)
+#Set appropriate baud rate
+sc.setBaud(115200)
+#Initialize the connection
+sc.initConnection()
+
 while True:
     PWM_L, PWM_R = get_PWMs(x_ref, curr_time, x_act, PWM_L, PWM_R)
-    # send to PWMs to Arduino
+    ##Send to PWMs to Arduino
+    #Use sc.write()
     time.sleep(timeslice)
     curr_time += timeslice
-    # wait for input from Arduino: wheel distances as dist_l and dist_r
+    ##Wait for input from Arduino: wheel distances as dist_l and dist_r
+    #Use sc.read()
     dist_l, dist_r = 0,0
     x_act_new = positions.get_x_act_new(x_act, dist_l, dist_r)
     x_act = x_act_new
