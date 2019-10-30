27a3d94564e10ed60ee81d80bd7832807e687e76
AlexLamson <alex.lamson@gmail.com>
2019-10-30 12:09:14 -0400

    serial communication works with 2 numbers both ways

--- arduino/simple_controller/simple_controller.ino
+++ arduino/simple_controller/simple_controller.ino
@@ -1,27 +1,45 @@
-void setup() {
-  Serial.begin(115200);
-  pinMode(13, OUTPUT);
-  Serial.println("arduino->pi");
-}
+long encodersLastSent;
 
-void loop() {
-  Serial.println("arduino->pi");
-  if(Serial.available()){
-    flash(Serial.parseInt());
-    Serial.flush();
-  }
-  delay(1000);
-}
 
 void flash(int n){
-  Serial.print("arduino->pi start");
-  Serial.println(n);
   for(int i=0;i<n;i++){
     digitalWrite(13,HIGH);
-    delay(500);
+    delay(50);
     digitalWrite(13,LOW);
-    delay(500);
-    Serial.println(i+1);
+    delay(20);
   }
-  Serial.println("arduino->pi end");
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  // Serial.begin(115200);
+  pinMode(13, OUTPUT);
+
+  encodersLastSent = millis();
+}
+
+
+void loop() {
+  // send encoder values to pi
+  if(millis() > encodersLastSent + 500) {
+    encodersLastSent = millis();
+
+    int leftEncoderValue = -1;
+    int rightEncoderValue = -1;
+
+    // send encoder values
+    Serial.print(leftEncoderValue);
+    Serial.print(" ");
+    Serial.print(rightEncoderValue);
+    Serial.println();
+  }
+
+  // receive motor commands
+  int leftMotorValue = Serial.parseInt();
+  int rightMotorValue = Serial.parseInt();
+  flash(leftMotorValue);
+
+  // TODO: remove this in the final program
+  delay(100);
 }
--- pi/simple_controller.py
+++ pi/simple_controller.py
@@ -1,35 +1,31 @@
 from serial import Serial
-# import serial.tools.list_ports.comports as get_ports
-from serial.tools.list_ports import comports as get_ports
+from serial.tools.list_ports import comports as get_serial_ports
 
 
 # connect to the open serial port
-ports = [p[0] for p in get_ports()]
+ports = [p[0] for p in get_serial_ports()]
 if len(ports) == 0:
     print("Error, couldn't find any open ports")
     exit()
-print("ports: {}".format(ports))
-
-# 115200
-ser = Serial(port=ports[0], baudrate=115200)
+ser = Serial(port=ports[0], baudrate=9600)
+# ser = Serial(port=ports[0], baudrate=115200)
 ser.flushInput()
 
-comp_list = ["arduino->pi end", "arduino->pi"]
-while True:
-	if ser.inWaiting() > 0:
-		inputValue = ser.readline().decode("utf-8").strip()
-		print(inputValue)
-		if inputValue in comp_list:
-			try:
-				n = input("Set arduino flash times:")
-				if n == "exit":
-					exit()
 
-				message = "{}".format(n)
-				to_write = bytearray(message.encode("ascii"))
-				ser.write(to_write)
-			except:
-				print("Input error, please input a number")
-				message = "{}".format(0)
-				to_write = bytearray(message.encode("ascii"))
-				ser.write(to_write)
+while True:
+	if ser.inWaiting():
+		# receive the encoder values
+		inputValue = ser.readline().decode("utf-8").strip()
+		if len(inputValue.split()) != 2:
+			print("error: input was '{}'".format(inputValue))
+			continue
+
+		left_ticks, right_ticks = inputValue.split()
+		print("arduino->pi: encoder: {} {}".format(left_ticks, right_ticks))
+
+		# send the new motor signals
+		left_motor, right_motor = 3, 3
+		print("pi->arduino: motor: {} {}".format(left_motor, right_motor))
+		message = "{} {}\n".format(left_motor, right_motor)
+		to_write = bytearray(message.encode("ascii"))
+		ser.write(to_write)
