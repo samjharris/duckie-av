0b8db9265e800e3d062467fdf544073ad1aa64f4
AlexLamson <alex.lamson@gmail.com>
2019-10-30 12:19:25 -0400

    reintroduce encoder code

--- arduino/simple_controller/simple_controller.ino
+++ arduino/simple_controller/simple_controller.ino
@@ -1,3 +1,15 @@
+// We assume that the two back encoders plugged into the interrupt pins
+// Note: interrupt pin 0 is actually pin 2
+// Note: interrupt pin 1 is actually pin 3
+
+#define ENCODER_PIN_BACK_LEFT 2
+#define ENCODER_PIN_BACK_RIGHT 3
+#define ENCODER_PIN_FRONT_LEFT 5
+#define ENCODER_PIN_FRONT_RIGHT 6
+
+volatile long left_encoder_counter = 0;
+volatile long right_encoder_counter = 0;
+
 long encodersLastSent;
 
 
@@ -11,10 +23,42 @@ void flash(int n){
 }
 
 
+void left_encoder_interrupt_function() {
+  bool back_left = (digitalRead(ENCODER_PIN_BACK_LEFT) == HIGH);
+  bool front_left = (digitalRead(ENCODER_PIN_FRONT_LEFT) == HIGH);
+
+  if((!back_left && !front_left) || (back_left && front_left)) {
+    // moving forward
+    left_encoder_counter += 1;
+  }
+  else if((!back_left && front_left) || (back_left && !front_left)) {
+    // moving backward
+    left_encoder_counter += -1;
+  }
+}
+
+void right_encoder_interrupt_function() {
+  bool back_right = (digitalRead(ENCODER_PIN_BACK_RIGHT) == HIGH);
+  bool front_right = (digitalRead(ENCODER_PIN_FRONT_RIGHT) == HIGH);
+
+  if((!back_right && !front_right) || (back_right && front_right)) {
+    // moving forward
+    right_encoder_counter += 1;
+  }
+  else if((!back_right && front_right) || (back_right && !front_right)) {
+    // moving backward
+    right_encoder_counter += -1;
+  }
+}
+
+
 void setup() {
   Serial.begin(9600);
   // Serial.begin(115200);
   pinMode(13, OUTPUT);
+
+  attachInterrupt(0, left_encoder_interrupt_function, CHANGE);
+  attachInterrupt(1, right_encoder_interrupt_function, CHANGE);
 
   encodersLastSent = millis();
 }
@@ -25,13 +69,10 @@ void loop() {
   if(millis() > encodersLastSent + 500) {
     encodersLastSent = millis();
 
-    int leftEncoderValue = -1;
-    int rightEncoderValue = -1;
-
     // send encoder values
-    Serial.print(leftEncoderValue);
+    Serial.print(left_encoder_counter);
     Serial.print(" ");
-    Serial.print(rightEncoderValue);
+    Serial.print(right_encoder_counter);
     Serial.println();
   }
 
