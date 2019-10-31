attempt to fix average theta bug

pi/positions.py
@@ -67,7 +67,12 @@ def get_x_act_new(x_act_prev, dist_l, dist_r):
     # print("delta_theta (should be ~0)", delta_theta)
     new_theta = x_act_prev[2] + delta_theta
     # print("new_theta (should be ~0)", new_theta)
-    avg_theta = x_act_prev[2] + delta_theta / 2
+
+    ys = np.sin(np.deg2rad(x_act_prev[2])) + np.sin(np.deg2rad(delta_theta))
+    xs = np.cos(np.deg2rad(x_act_prev[2])) + np.cos(np.deg2rad(delta_theta))
+    avg_theta = np.arctan2(ys, xs)
+
+    # avg_theta = x_act_prev[2] + delta_theta / 2
     # print("avg_theta (should be ~0)", avg_theta)
 
     # TODO update with better approximation