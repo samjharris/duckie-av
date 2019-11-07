b9fc5f3d47e8ff307049bc4e42f6b976791e9f50
AlexLamson <alex.lamson@gmail.com>
2019-11-07 10:38:45 -0500

    this script will replace simple serial

---
+++ pi/simple_command.py
@@ -0,0 +1,5 @@
+def compute_motor_values(t, delta_t, left_encoder, right_encoder, delta_left_encoder, delta_right_encoder):
+    print(left_encoder, right_encoder)
+    # print("go forward!")
+    return 400, 400
+    # return 0, 0
