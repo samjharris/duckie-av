fixed bug with PWM scaling

pi/odometry_guided_feedback.py
@@ -53,7 +53,7 @@ def get_PWMs(x_ref_func, t, dt, x_act, x_act_prev, PWM_L_prev, PWM_R_prev):
     # useful comment
     delta_x_act = x_act_prev - x_act
 
-    sign =0
+    sign = 0
     dir_vel_vec = np.dot(x_spring, delta_x_act[:2])
     if(np.linalg.norm(dir_vel_vec) == 0):
         sign = 1
@@ -71,6 +71,8 @@ def get_PWMs(x_ref_func, t, dt, x_act, x_act_prev, PWM_L_prev, PWM_R_prev):
 
     spring_displacement = np.linalg.norm(x_spring)
 
+    if spring_displacement == 0:
+        return (PWM_L_prev, PWM_R_prev)
     # Unit vector in direction from yoke to x_ref
     x_unit_spring = x_spring / spring_displacement
 
@@ -105,25 +107,15 @@ def get_PWMs(x_ref_func, t, dt, x_act, x_act_prev, PWM_L_prev, PWM_R_prev):
     # Add delta PWMs to previous values to obtain new values
     # Subtract rotational term from left and add to right (right hand rule)
 
-    # print("{:>22} : {}".format("PWM_L_new before scaling", PWM_L_new))
-    # print("{:>22} : {}".format("PWM_R_new before scaling", PWM_R_new))
+    max_vel_allowed = convert_PWM_to_vel(400)
+    max_of_vel_news = max(abs(vel_L_new), abs(vel_R_new))
 
-    reduction_coefficient = 1
-    max_vel = convert_PWM_to_vel(400)
-    if abs(vel_L_new) > max_vel:
-        reduction_coefficient = max_vel/abs(vel_L_new)
-    if abs(vel_R_new) > max_vel:
-        right_reduction_coefficient = max_vel/abs(vel_R_new)
-        reduction_coefficient = min(right_reduction_coefficient, reduction_coefficient)
+    if max_of_vel_news > max_vel_allowed:
+        vel_L_new = max_vel_allowed * vel_L_new / max_of_vel_news
+        vel_R_new = max_vel_allowed * vel_R_new / max_of_vel_news
 
-    PWM_L_new = convert_vel_to_PWM(vel_L_new)
-    PWM_R_new = convert_vel_to_PWM(vel_R_new)
-
-    PWM_L_new = int(reduction_coefficient * PWM_L_new)
-    PWM_R_new = int(reduction_coefficient * PWM_R_new)
-
-    # print("{:>22} : {}".format("PWM_L_new after scaling", PWM_L_new))
-    # print("{:>22} : {}".format("PWM_R_new after scaling", PWM_R_new))
+    PWM_L_new = int(convert_vel_to_PWM(vel_L_new))
+    PWM_R_new = int(convert_vel_to_PWM(vel_R_new))
 
     def print_debug_info():
         print("{:>22} : {}".format("K", K))
@@ -147,6 +139,7 @@ def get_PWMs(x_ref_func, t, dt, x_act, x_act_prev, PWM_L_prev, PWM_R_prev):
         print("{:>22} : {}".format("vel_R_prev", vel_R_prev))
         print("{:>22} : {}".format("vel_L_new", vel_L_new))
         print("{:>22} : {}".format("vel_R_new", vel_R_new))
+        print("{:>22} : {}".format("max_vel_allowed", max_vel_allowed))
         print("{:>22} : {}".format("PWM_L_new", PWM_L_new))
         print("{:>22} : {}".format("PWM_R_new", PWM_R_new))
         print("="*30)