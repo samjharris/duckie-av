97b484eded1a114eeba9f551fc6264752d91c59c
Alex Behr <abehr@umass.edu>
2019-11-12 17:01:43 -0500

    switched to arctan2, added PWM to vel conversion

--- pi/visual_controller.py
+++ pi/visual_controller.py
@@ -1,7 +1,7 @@
 # using equation theta_2 = -K (theta - theta_ref) - B (theta_dot - theta_1_ref)
 # theta_dot = theta_(t+1) - theta_t
 from odometry_guided_feedback import convert_PWM_to_vel, convert_vel_to_PWM
-from config import K, B
+from config import *
 import numpy as np
 from collections import deque
 
@@ -13,14 +13,14 @@ previous_dts = deque()
 #   PWM_r_prev: float,
 #   lane_error_pix: int,
 #   time since last call, dt: float, 
-#   stop_marker: bool
+#   stop_marker_seen: bool
 # returns 
 #   (PWM_l, PWM_r): (float, float)
-def get_PWMs_from_visual(lane_error_pix, dt, stop_marker, PWM_l_prev, PWM_r_prev):
+def get_PWMs_from_visual(lane_error_pix, dt, stop_marker_seen, PWM_l_prev, PWM_r_prev):
     # TODO: start and proceed at speed limit --- Maybe not here
 
     # TODO: stop on red boolean
-    if stop_marker:
+    if stop_marker_seen:
         return 0, 0
 
     # TODO: translate pixel error from center of bot to center of lane to theta
@@ -29,7 +29,7 @@ def get_PWMs_from_visual(lane_error_pix, dt, stop_marker, PWM_l_prev, PWM_r_prev
     DIST_TO_ROI_CM = 10
     PIX_PER_CM = 100
     lane_error_cm = lane_error_pix / PIX_PER_CM
-    theta = np.arctan(lane_error_cm / DIST_TO_ROI_CM)
+    theta = np.arctan2(lane_error_cm, DIST_TO_ROI_CM)
 
     # TODO: store past thetas and calculate moving average theta_dot
     previous_thetas.append(theta)
@@ -38,15 +38,24 @@ def get_PWMs_from_visual(lane_error_pix, dt, stop_marker, PWM_l_prev, PWM_r_prev
         previous_thetas.popleft()
         previous_dts.popleft()
     avg_theta = sum(previous_thetas) / len(previous_thetas)
+    # TODO: better calculation: computer theta vel for each dt and then average
     theta_velocity = avg_theta / sum(previous_dts)
 
     # TODO: use equation to determine delta_PWM (delta_PWM ~ theta_acceleration)
     delta_PWM = - K * theta - B * theta_velocity
-    
+
+    # handle PWM <==> velocity stuff  
+    vel_l_prev = convert_PWM_to_vel(PWM_l_prev)
+    vel_r_prev = convert_PWM_to_vel(PWM_r_prev)
+    if delta_PWM > 0:
+        delta_PWM += MIN_PWM
+    if delta_PWM < 0:
+        delta_PWM -= MIN_PWM
+    delta_vel = convert_PWM_to_vel(delta_PWM)
+
     # TODO: return PWMs
-    # globals??
-    PWM_l = PWM_l_prev - delta_PWM
-    PWM_r = PWM_r_prev + delta_PWM
+    PWM_l = convert_vel_to_PWM(vel_l_prev - delta_vel)
+    PWM_r = convert_vel_to_PWM(vel_r_prev + delta_vel)
 
     return PWM_l, PWM_r
 
@@ -55,4 +64,4 @@ def clear_visual_globals():
     previous_dts.clear()
 
 def test():
-    return get_PWMs_from_visual(150, 150, 20, 0.1, False)
+    return get_PWMs_from_visual(20, 0.1, False, 150, 150)
