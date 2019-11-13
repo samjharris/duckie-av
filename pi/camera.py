e4b18364491456e09f30c8eef986357182ddb993
AlexLamson <alex.lamson@gmail.com>
2019-11-13 17:50:50 -0500

    fix misunderstanding in how frames are captured

--- pi/camera.py
+++ pi/camera.py
@@ -2,7 +2,7 @@ from tqdm import tqdm
 # import io
 from time import sleep
 from PIL import Image
-from threading import Lock
+from threading import Lock, Thread
 import picamera
 import numpy as np
 import picamera.array
@@ -24,28 +24,35 @@ class Camera:
         self.camera.awb_mode = "off"
         self.camera.awb_gains = g
         
-        self.stream = picamera.array.PiRGBArray(self.camera, size=(self.width, self.height))
-        self.raw_buffer = picamera.array.PiRGBArray(self.camera, size=(self.width, self.height))
+        self.raw_capture = picamera.array.PiRGBArray(self.camera, size=(self.width, self.height))
         self.lock = Lock()
         self.cur_error = 0
         self.should_stop = False
 
 
-    def process_image(self, frame):
+    def process_frame(self, frame):
         #process image here
         error = get_pixel_error_from_image(frame)
         print(error)
         return error
 
 
+    def start_capture_async(self):
+        video_thread = Thread(target=self.start_capture)
+        video_thread.start()
+
+
     def start_capture(self):
-        for iteration in self.camera.capture_continuous(self.stream, format="rgb", use_video_port=True):
+        stream = self.camera.capture_continuous(self.raw_capture, format="rgb", use_video_port=True)
+        for f in stream:
             if(self.should_stop):
                 return
-            self.stream.truncate()
-            self.stream.seek(0)
-            ## get the error of the current frame
-            error = self.process_image(self.stream)
+            self.raw_capture.truncate(0)
+
+            # process the frame
+            error = self.process_frame(f.array)
+
+            # update the pixel error (instance attribute)
             self.lock.acquire()
             self.cur_error = error
             self.lock.release()
