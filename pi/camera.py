9309757dfa4d0f348ad7a3b64393f58655fdc50f
AlexLamson <alex.lamson@gmail.com>
2019-11-13 17:09:27 -0500

    fix compilation errors in experimental camera changes

--- pi/camera.py
+++ pi/camera.py
@@ -6,56 +6,54 @@ from threading import Lock
 import picamera
 import numpy as np
 import picamera.array
+from image_interpreter import get_pixel_error_from_image
 
-class camera:
+
+class Camera:
     def __init__(self, width=640, height=480):
-        ## initialize picamera object
-        self.cam = picamera.PiCamera()
-        camera.resolution = (width, height)
-        #camera.framerate = 80
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (width, height)
+        # self.camera.framerate = 80
+        
         ## wait for exposure/gain to adjust
         sleep(2)
         ## set exposure/gain for consistent images
-        self.cam.shutter_speed = self.cam.exposure_speed
-        self.cam.exposure_mode = "off"
-        g = self.cam.awb_gains
-        self.cam.awb_mode = "off"
-        self.cam.awb_gains = g
-        ## initialize the stream buffer
-        self.stream = picamera.array.PiRGBArray(self.cam, size=(self.width, self.height))
-        ## initialize the raw buffer
-        self.raw = picamera.array.PiRGBArray(self.cam, size=(self.width, self.height))
-        ## initialize the lock 
-        self.lock = threading.Lock()
-        ## initialize the current error to 0
+        self.camera.shutter_speed = self.camera.exposure_speed
+        self.camera.exposure_mode = "off"
+        g = self.camera.awb_gains
+        self.camera.awb_mode = "off"
+        self.camera.awb_gains = g
+        
+        self.stream = picamera.array.PiRGBArray(self.camera, size=(self.width, self.height))
+        self.raw_buffer = picamera.array.PiRGBArray(self.camera, size=(self.width, self.height))
+        self.lock = Lock()
         self.cur_error = 0
-        ## initialize the stop signal
-        self.stop = False
-        return
-    
-    def start_capture():
-        for iteration in camera.capture_continuous(self.stream, format="rgb", use_video_port=True)
-            if(stop):
+        self.should_stop = False
+
+
+    def process_image(self, frame):
+        #process image here
+        error = get_pixel_error_from_image(frame)
+        print(error)
+        return error
+
+
+    def start_capture(self):
+        for iteration in self.camera.capture_continuous(self.stream, format="rgb", use_video_port=True):
+            if(self.should_stop):
                 return
             self.stream.truncate()
             self.stream.seek(0)
             ## get the error of the current frame
-            error = process_image(self.stream)
+            error = self.process_image(self.stream)
             self.lock.acquire()
             self.cur_error = error
             self.lock.release()
-    
-    def process_image(frame):
-        #process image here
-        img = Image.fromarray(frame, 'RGB')
 
-        return 0
 
-    def get_error():
+    def get_error(self):
         self.lock.acquire()
         error = self.cur_error
         self.lock.release()
+        self.camera.close()
         return error
-
-
-
