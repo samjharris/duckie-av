d989672822aaea5fc80f1bbdd313cc0de9b853dc
Alex Behr <abehr@umass.edu>
2019-11-14 17:03:22 -0500

    made all magic numbers floats to address int division issue

--- pi/config.py
+++ pi/config.py
@@ -5,28 +5,28 @@
 CM_S_PWM = 0.15
 
 # minimum PWM required to move the robot
-MIN_PWM = 55 #prev min_pwm
+MIN_PWM = 55.0 #prev min_pwm
 
 # mass of the robot (kilograms)
 ROBOT_MASS = 0.830 #
 
 # yoke point distance from center of the wheel base (centimeters)
-YOKE_POINT = 5 #prev r_length
+YOKE_POINT = 5.0 #prev r_length
 
 # K of 0.5, B of 3 and I of 30 work well for straight
 K = 0.1  # spring constant
-B = 0  # damper constant
-I = 5    # length of torque arm
+B = 0.0  # damper constant
+I = 5.0  # length of torque arm
 
-DIST_TO_ROI_CM = 15 # distance from bot to center of ROI
-PIX_PER_CM = 131 / 20.25 # pixels per cm in ROI
+DIST_TO_ROI_CM = 15.0 # distance from bot to center of ROI
+PIX_PER_CM = 131.0 / 20.25 # pixels per cm in ROI
 
 TIME_SLICE = 0.1 # fraction of a second; resolution of our function
-END_GOAL = 100 # goal is one meter measured in centimeters
-STRAIGHT_SPEED_LIMIT = 15 # cm/s
-TURN_SPEED_LIMIT = 10 # cm/s
+END_GOAL = 100.0 # goal is one meter measured in centimeters
+STRAIGHT_SPEED_LIMIT = 15.0 # cm/s
+TURN_SPEED_LIMIT = 10.0 # cm/s
 
-CIRCLE_RADIUS = 30
+CIRCLE_RADIUS = 30.0
 
 WHEEL_BASE = 16.5
 # BAUDRATE = 9600
