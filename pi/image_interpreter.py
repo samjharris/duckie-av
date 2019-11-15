b6f2e62b6a98e867651e5fc7e686e471c5a3a76f
ch403 <cwhuang@umass.edu>
2019-11-15 15:15:25 -0500

    takes the functions in get_pixel_error_from_image outside

--- pi/image_interpreter.py
+++ pi/image_interpreter.py
@@ -22,132 +22,125 @@ parent_dir = os.path.dirname(os.getcwd())
 image_path = os.path.join(parent_dir, 'test_road_images/')
 #image_path = os.path.join(parent_dir, 'test_road_images\\')
 
+def RGBtoHSV(rgb):
+    r = rgb[0] / 255.0
+    g = rgb[1] / 255.0
+    b = rgb[2] / 255.0
+    h = 0
+    s = 0
+    v = 0
+    mn = min(r,g,b)
+    mx = max(r,g,b) 
+    dc = mx - mn
 
+    #calculate Hue (unit: degrees)
+    if(dc == 0):
+        h = 0
+    elif(r == mx):
+        h = 60 * (((g - b)/dc) % 6)
+    elif(g == mx):
+        h = 60 * (((b - r)/dc) + 2)
+    else:
+        h = 60 * (((r - g)/dc) + 4)
+
+    #calculate Saturation (unit: pct)
+    if(mx == 0):
+        s = 0
+    else:
+        s = dc / mx
+
+    #calculate Value (unit: pct)
+    v = mx
+
+    h = h * (255.0/360)
+    s = s * (255.0)
+    v = v * (255.0)
+
+    return (int(h),int(s),int(v))
+
+#check if a pixel is black
+def isBlack(hsv_color):
+    h, s, v = hsv_color
+    # from testing various sample images, black 
+    # seems to be v<40%, so 40% * 255 =  102 
+    if v < 102:
+        return 1
+    return 0
+
+#For yellow color a hue range from 51 degree to 60 degree has been defined
+def isYellow(hsv_color):
+    h, s, v = hsv_color
+    if 30 <= h <= 60:
+        if 100 <= s <=255:
+            return 255
+        else:
+            return 0
+    else:
+        return 0
+
+def isWhite(hsv_color):
+    h, s, v = hsv_color
+    if 0 <= s <= 15:
+        if 240 <= v <=255:
+            return 255
+        else:
+            return 0
+    else:
+        return 0
+
+def isRed(hsv_color):
+    h,s,v = hsv_color
+    if 240 <= h <= 255:
+        if 125 <= s <=255:
+            return 1
+        else:
+            return 0
+    else:
+        return 0
 
 def get_pixel_error_from_image(frame):
     #t = time.time_ns()
-    
-    #if(not init_roi):
-    #    init_roi = True
-
     image_in = Image.fromarray(frame)
-    # numimg = np.array(image_in)
-    # convert to hue
-    # hsv = image_in.convert('HSV')
-    # pixel_val_hsv = np.array(hsv)
     height, width, depth = frame.shape
 
     # look for specific range for different colors to define roi
-    
     a = frame[height//2-(int(height*crop_percentage)):height//2+(int(height*crop_percentage)), : , :]
     
-    # print(a.shape)
     # # to display an image
     # b = Image.fromarray(a, 'HSV')
-    b = Image.fromarray(a[:,:], 'RGB')
+    # b = Image.fromarray(a[:,:], 'RGB')
     # c = b.convert('RGB')
     # c.save(image_path + 'strtA_crop15perc.jpg')
-    b.show()
-
-    #convert one RGB pixel to HSV
-    def RGBtoHSV(rgb):
-        r = rgb[0] / 255.0
-        g = rgb[1] / 255.0
-        b = rgb[2] / 255.0
-        h = 0
-        s = 0
-        v = 0
-        mn = min(r,g,b)
-        mx = max(r,g,b) 
-        dc = mx - mn
-
-        #calculate Hue (unit: degrees)
-        if(dc == 0):
-            h = 0
-        elif(r == mx):
-            h = 60 * (((g - b)/dc) % 6)
-        elif(g == mx):
-            h = 60 * (((b - r)/dc) + 2)
-        else:
-            h = 60 * (((r - g)/dc) + 4)
-
-        #calculate Saturation (unit: pct)
-        if(mx == 0):
-            s = 0
-        else:
-            s = dc / mx
-
-        #calculate Value (unit: pct)
-        v = mx
-
-        h = h * (255.0/360)
-        s = s * (255.0)
-        v = v * (255.0)
-
-        return (int(h),int(s),int(v))
-
-    #check if a pixel is black
-    def isBlack(hsv_color):
-        h, s, v = hsv_color
-        # from testing various sample images, black 
-        # seems to be v<40%, so 40% * 255 =  102 
-        if v < 102:
-            return 1
-        return 0
-
-    #For yellow color a hue range from 51 degree to 60 degree has been defined
-    def isYellow(hsv_color):
-        h, s, v = hsv_color
-        if 30 <= h <= 60:
-            if 100 <= s <=255:
-                return 255
-            else:
-                return 0
-        else:
-            return 0
-
-    def isWhite(hsv_color):
-        h, s, v = hsv_color
-        if 0 <= s <= 15:
-            if 240 <= v <=255:
-                return 255
-            else:
-                return 0
-        else:
-            return 0
-
-    def isRed(hsv_color):
-        h,s,v = hsv_color
-        if 240 <= h <= 255:
-            if 125 <= s <=255:
-                return 1
-            else:
-                return 0
-        else:
-            return 0
+    # b.show()
 
     yellowStrip = np.zeros((a.shape[0],a.shape[1]//down_sample_steps),dtype=a.dtype)
     whiteStrip = np.zeros((a.shape[0],a.shape[1]//down_sample_steps),dtype=a.dtype)
     redStrip = np.zeros((a.shape[0],a.shape[1]//down_sample_steps),dtype=a.dtype)
 
+
+    if(not init_roi):
+       init_roi = True
+       yellow_roi, white_roi, red_roi = find_rois(a)
+
+    
 # =============================================================================
 #     reducedImage = np.zeros((a.shape[0],a.shape[1]//4,a.shape[2]),dtype=a.dtype)
 # =============================================================================
-    cnt = 0
+    # cnt = 0
     for M in range(whiteStrip.shape[0]):
         for N in range(whiteStrip.shape[1]):
-            pixel = RGBtoHSV(a[M,down_sample_steps*N])
+            pixel = RGBtoHSV(a[M,down_sample_steps*N]) #convert one RGB pixel to HSV
             #ignore black pixels 
             if(isBlack(pixel)):
-                continue
-            if(isWhite(pixel)):
-                whiteStrip[M,N] = 255
                 continue
             if(isYellow(pixel)):
                 yellowStrip[M,N] = 255
                 continue
             if(isRed(pixel)):
                 redStrip[M,N] = 1
+                continue
+            if(isWhite(pixel)):
+                whiteStrip[M,N] = 255
                 continue
 
 # =============================================================================
