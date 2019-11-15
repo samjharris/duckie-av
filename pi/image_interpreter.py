3dd954136e3ee9b2b9c77afbd47f0d8e2747fecc
Sam Harris <sjharris@umass.edu>
2019-11-15 10:19:24 -0500

    added check for (not back)

--- pi/image_interpreter.py
+++ pi/image_interpreter.py
@@ -8,6 +8,9 @@ import os
 import PIL.Image as Image
 import numpy as np
 
+# testing imports
+#import time
+
 # variables
 crop_percentage = 0.05
 down_sample_steps = 8
@@ -16,12 +19,15 @@ down_sample_steps = 8
 # link to images
 parent_dir = os.path.dirname(os.getcwd())
 image_path = os.path.join(parent_dir, 'test_road_images/')
+#image_path = os.path.join(parent_dir, 'test_road_images\\')
 
 
 
 def get_pixel_error_from_image(frame):
-    if(!init_roi):
-        init_roi = True
+    #t = time.time_ns()
+    
+    #if(not init_roi):
+    #    init_roi = True
 
     image_in = Image.fromarray(frame)
     # numimg = np.array(image_in)
@@ -31,7 +37,9 @@ def get_pixel_error_from_image(frame):
     height, width, depth = frame.shape
 
     # look for specific range for different colors to define roi
+    
     a = frame[height//2-(int(height*crop_percentage)):height//2+(int(height*crop_percentage)), : , :]
+    
     # print(a.shape)
     # # to display an image
     # b = Image.fromarray(a, 'HSV')
@@ -47,13 +55,13 @@ def get_pixel_error_from_image(frame):
         r = rgb[0] / 255.0
         g = rgb[1] / 255.0
         b = rgb[2] / 255.0
-        mn = min(r,g,b)
-        mx = max(r,g,b) 
-        dc = mx - mn
         h = 0
         s = 0
         v = 0
-        
+        mn = min(r,g,b)
+        mx = max(r,g,b) 
+        dc = mx - mn
+
         #calculate Hue (unit: degrees)
         if(dc == 0):
             h = 0
@@ -81,7 +89,12 @@ def get_pixel_error_from_image(frame):
 
     #check if a pixel is black
     def isBlack(hsv_color):
-        return 1 if v < 20 else 0
+        h, s, v = hsv_color
+        # from testing various sample images, black 
+        # seems to be v<40%, so 40% * 255 =  102 
+        if v < 102:
+            return 1
+        return 0
 
     #For yellow color a hue range from 51 degree to 60 degree has been defined
     def isYellow(hsv_color):
@@ -93,8 +106,6 @@ def get_pixel_error_from_image(frame):
                 return 0
         else:
             return 0
-
-    
 
     def isWhite(hsv_color):
         h, s, v = hsv_color
@@ -116,17 +127,21 @@ def get_pixel_error_from_image(frame):
         else:
             return 0
 
-
     yellowStrip = np.zeros((a.shape[0],a.shape[1]//down_sample_steps),dtype=a.dtype)
     whiteStrip = np.zeros((a.shape[0],a.shape[1]//down_sample_steps),dtype=a.dtype)
     redStrip = np.zeros((a.shape[0],a.shape[1]//down_sample_steps),dtype=a.dtype)
+
 # =============================================================================
 #     reducedImage = np.zeros((a.shape[0],a.shape[1]//4,a.shape[2]),dtype=a.dtype)
 # =============================================================================
+    cnt = 0
     for M in range(whiteStrip.shape[0]):
         for N in range(whiteStrip.shape[1]):
 
             pixel = RGBtoHSV(a[M,down_sample_steps*N])
+            #ignore black pixels 
+            if(isBlack(pixel)):
+                continue
             whiteStrip[M,N] = isWhite(pixel)
             yellowStrip[M,N] = isYellow(pixel)
             redStrip[M,N] = isRed(pixel)
@@ -140,6 +155,7 @@ def get_pixel_error_from_image(frame):
 # 
 #     calculate the distance of lane center and image center
 # =============================================================================
+
     yelColSum = np.sum(yellowStrip, axis=0)
     yelEdge = np.argmax(yelColSum)
 
@@ -163,7 +179,7 @@ def get_pixel_error_from_image(frame):
     if redRowSum[10] >= (0.4*redStrip.shape[1]):
         saw_red = True
 
-
+    #dt = time.time_ns()
 
     return (error, saw_red)
 
@@ -183,7 +199,8 @@ def get_pixel_error_from_image(frame):
 
 if __name__ == "__main__":
     # read in image
-    image_in = Image.open(image_path + 'dist_to_red_15cm.png', 'r')
+    image_in = Image.open(image_path + 'frame1.png', 'r')
     rgb_frame = np.array(image_in)
+
     error = get_pixel_error_from_image(rgb_frame)
     print(error)
