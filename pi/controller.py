64c0d1983ada81de09d98a8d1fb90a75843c2e03
samjharris <sjharris@umass.edu>
2019-12-05 16:23:19 -0500

    moved cam object

--- pi/controller.py
+++ pi/controller.py
@@ -11,7 +11,6 @@ from serial.tools.list_ports import comports as get_serial_ports
 import struct
 from time import time, sleep
 from visual_control import visual_compute_motor_values, convert_vel_to_PWM
-from camera import Camera
 from open_control import open_compute_motor_values
 from path_planner import plan_path
 
@@ -42,7 +41,7 @@ class Controller():
 
         #compute motor values
         if(self.control_type == CONTROL_VISUAL): #visual control
-            l_motor,r_motor,saw_red,saw_green = visual_compute_motor_values(t, delta_t, l_encod, r_encod, delta_l_encod, delta_r_encod, l_motor_prev, r_motor_prev, self.hug, cam)
+            l_motor,r_motor,saw_red,saw_green = visual_compute_motor_values(t, delta_t, l_encod, r_encod, delta_l_encod, delta_r_encod, l_motor_prev, r_motor_prev, self.hug)
         elif(self.control_type == CONTROL_OPEN): #open-loop control
             l_motor,r_motor,done = open_compute_motor_values(self.prev_hug, self.instruction, delta_l_encod, delta_r_encod)
         else: #self.control_type == CONTROL_STOP we are "stopped"
@@ -103,9 +102,6 @@ if __name__ == "__main__":
         # Instantiate our controller
         cont = Controller(instructions, full_path)
 
-        # Instantiate our controller
-        cam = Camera()
-
         # These variables will hold the incoming serial data
         bytes_buffer = b""
         buffer_i = 0
@@ -153,7 +149,7 @@ if __name__ == "__main__":
                         delta_r_encod = r_encod - r_encod_prev
 
                         # ask the controller what to do
-                        l_motor, r_motor = cont.compute_motor_values(t, delta_t, l_encod, r_encod, delta_l_encod, delta_r_encod, l_motor_prev, r_motor_prev,cam)
+                        l_motor, r_motor = cont.compute_motor_values(t, delta_t, l_encod, r_encod, delta_l_encod, delta_r_encod, l_motor_prev, r_motor_prev)
                         #l_motor,r_motor,done = open_compute_motor_values(HUG_WHITE, "left", delta_l_encod, delta_r_encod)
                         
                         # do what the controller said to do
